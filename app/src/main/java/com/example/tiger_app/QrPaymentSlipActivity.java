cbee31855e749142823dfb261a71d08b30c5f0c8
canmononui <canmononui_tuinui@hotmail.co.th>
2022-01-10 18:23:23 +0700

    Qr Payment

---
+++ app/src/main/java/com/example/tiger_app/QrPaymentSlipActivity.java
@@ -0,0 +1,14 @@
+package com.example.tiger_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrPaymentSlipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_payment_slip);
+    }
+}
